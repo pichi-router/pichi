cd182163dbc90b1d49b00ed89ad9b57cbb373ca8
Pichi <pichi-router@pm.me>
2018-09-10 05:49:11 -0100

    Make cmake files to be compatible with Microsoft vcpkg
    - all libraries should have 'lib' prefix
    - libmaxminddb header file contains 'maxminddb' prefix
    - reduce finding boost

--- cmake/FindRapidjson.cmake
+++ cmake/FindRapidjson.cmake
@@ -1,7 +1,7 @@
-find_path(Rapidjson_INCLUDE_DIR NAMES rapidjson/rapidjson.h)
+find_path(Rapidjson_INCLUDE_DIRS NAMES rapidjson/rapidjson.h)
 
-if (Rapidjson_INCLUDE_DIR)
-  file(STRINGS "${Rapidjson_INCLUDE_DIR}/rapidjson/rapidjson.h" major_version_line
+if (Rapidjson_INCLUDE_DIRS)
+  file(STRINGS "${Rapidjson_INCLUDE_DIRS}/rapidjson/rapidjson.h" major_version_line
         REGEX "^#define[\t ]+RAPIDJSON_MAJOR_VERSION[\t ]+[0-9]*")
   if (major_version_line)
     string(REGEX REPLACE "^#define[\t ]+RAPIDJSON_MAJOR_VERSION[\t ]+([0-9]*)"
@@ -9,7 +9,7 @@ if (Rapidjson_INCLUDE_DIR)
     unset(major_version_line)
   endif (major_version_line)
 
-  file(STRINGS "${Rapidjson_INCLUDE_DIR}/rapidjson/rapidjson.h" minor_version_line
+  file(STRINGS "${Rapidjson_INCLUDE_DIRS}/rapidjson/rapidjson.h" minor_version_line
         REGEX "^#define[\t ]+RAPIDJSON_MINOR_VERSION[\t ]+[0-9]*")
   if (minor_version_line)
     string(REGEX REPLACE "^#define[\t ]+RAPIDJSON_MINOR_VERSION[\t ]+([0-9]*)"
@@ -17,7 +17,7 @@ if (Rapidjson_INCLUDE_DIR)
     unset(minor_version_line)
   endif (minor_version_line)
 
-  file(STRINGS "${Rapidjson_INCLUDE_DIR}/rapidjson/rapidjson.h" patch_version_line
+  file(STRINGS "${Rapidjson_INCLUDE_DIRS}/rapidjson/rapidjson.h" patch_version_line
         REGEX "^#define[\t ]+RAPIDJSON_PATCH_VERSION[\t ]+[0-9]*")
   if (patch_version_line)
     string(REGEX REPLACE "^#define[\t ]+RAPIDJSON_PATCH_VERSION[\t ]+([0-9]*)"
@@ -31,14 +31,10 @@ if (Rapidjson_INCLUDE_DIR)
   unset(major_version)
   unset(minor_version)
   unset(patch_version)
-endif (Rapidjson_INCLUDE_DIR)
+endif (Rapidjson_INCLUDE_DIRS)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Rapidjson
-                                  REQUIRED_VARS Rapidjson_INCLUDE_DIR Rapidjson_VERSION_STRING
-                                  VERSION_VAR Rapidjson_VERSION_STRING)
-set(Rapidjson_FOUND ${Rapidjson_FOUND})
-
-if (Rapidjson_FOUND)
-  set(Rapidjson_INCLUDE_DIRS ${Rapidjson_INCLUDE_DIR})
-endif (Rapidjson_FOUND)
+  REQUIRED_VARS Rapidjson_INCLUDE_DIRS Rapidjson_VERSION_STRING
+  VERSION_VAR Rapidjson_VERSION_STRING
+)
