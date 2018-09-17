d46d54b8d6c69d7e7ef6649ce445ff4730a5b951
Pichi <pichi@elude.in>
2018-09-17 09:56:15 +0100

    Replace 'string const&' with string_view in asserts

--- include/pichi/asserts.hpp
+++ include/pichi/asserts.hpp
@@ -2,13 +2,13 @@
 #define PICHI_ASSERTS_HPP
 
 #include <pichi/exception.hpp>
-#include <string>
+#include <string_view>
 
 namespace pichi {
 
-[[noreturn]] extern void fail(PichiError e, std::string const& msg = "");
-extern void assertTrue(bool b, PichiError e, std::string const& msg = "");
-extern void assertFalse(bool b, PichiError e, std::string const& msg = "");
+[[noreturn]] extern void fail(PichiError e, std::string_view msg = "");
+extern void assertTrue(bool b, PichiError e, std::string_view msg = "");
+extern void assertFalse(bool b, PichiError e, std::string_view msg = "");
 
 } // namespace pichi
 
--- src/asserts.cpp
+++ src/asserts.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 namespace pichi {
 
-[[noreturn]] void fail(PichiError e, string const& msg) {
+[[noreturn]] void fail(PichiError e, string_view msg) {
   throw Exception{e, msg};
 }
 
-void assertTrue(bool b, PichiError e, string const& msg)
+void assertTrue(bool b, PichiError e, string_view msg)
 {
   if (!b) fail(e, msg);
 }
 
-void assertFalse(bool b, PichiError e, string const& msg) { assertTrue(!b, e, msg); }
+void assertFalse(bool b, PichiError e, string_view msg) { assertTrue(!b, e, msg); }
 
 } // namespace pichi
