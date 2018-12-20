52049aa0d8d1b53afcf3b89953bb18cd17e7489b
Pichi <pichi-router@pm.me>
2018-12-20 22:53:20 +0400

    provide C interface to run pichi server in process

--- server/main.cpp
+++ server/main.cpp
@@ -1,14 +1,9 @@
-#include <boost/asio/io_context.hpp>
 #include <boost/program_options.hpp>
 #include <iostream>
-#include <pichi/api/server.hpp>
+#include <pichi.h>
 
 using namespace std;
-namespace api = pichi::api;
-namespace asio = boost::asio;
 namespace po = boost::program_options;
-
-static auto io = asio::io_context{1};
 
 int main(int argc, char const* argv[])
 {
@@ -17,9 +12,9 @@ int main(int argc, char const* argv[])
   auto geo = string{};
   auto desc = po::options_description{"Allow options"};
   desc.add_options()("help,h", "produce help message")(
-      "listen,l", po::value<string>(&listen)->default_value("::1"),
-      "API server address")("port,p", po::value<uint16_t>(&port),
-                            "API server port")("geo,g", po::value<string>(&geo), "GEO file");
+      "listen,l", po::value<string>(&listen)->default_value(PICHI_DEFAULT_BIND),
+      "API server address")("port,p", po::value<uint16_t>(&port), "API server port")(
+      "geo,g", po::value<string>(&geo)->default_value(PICHI_DEFAULT_MMDB), "GEO file");
   auto vm = po::variables_map{};
   po::store(po::parse_command_line(argc, argv, desc), vm);
   po::notify(vm);
@@ -29,9 +24,5 @@ int main(int argc, char const* argv[])
     return 1;
   }
 
-  auto server = api::Server{io, geo.c_str()};
-  server.listen(listen, port);
-
-  io.run();
-  return 0;
+  return pichi_run_server(listen.c_str(), port, geo.c_str());
 }
