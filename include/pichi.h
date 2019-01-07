bd0a87a886f9e2aa9ab52567997c2bbaf5078d67
Pichi <pichi@elude.in>
2019-01-07 21:24:34 +0700

    provide C interface to run pichi server in process

---
+++ include/pichi.h
@@ -0,0 +1,26 @@
+#ifndef PICHI_H
+#define PICHI_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+extern char const* PICHI_DEFAULT_BIND;
+extern char const* PICHI_DEFAULT_MMDB;
+
+/*
+ * Start PICHI server according to
+ *   - bind: server listening address, PICHI_DEFAULT_BIND if NULL,
+ *   - port: server listening port,
+ *   - mmdb: IP GEO database, MMDB format, PICHI_DEFAULT_MMDB if NULL.
+ * The function doesn't return if no error occurs, otherwise -1.
+ */
+extern int pichi_run_server(char const* bind, uint16_t port, char const* mmdb);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // PICHI_H
---
+++ src/pichi.cpp
@@ -0,0 +1,27 @@
+#include <boost/asio/io_context.hpp>
+#include <pichi.h>
+#include <pichi/api/server.hpp>
+
+using namespace std;
+namespace api = pichi::api;
+namespace asio = boost::asio;
+
+static asio::io_context io{1};
+
+char const* PICHI_DEFAULT_BIND = "::1";
+char const* PICHI_DEFAULT_MMDB = "/usr/share/pichi/geo.mmdb";
+
+int pichi_run_server(char const* bind, uint16_t port, char const* mmdb)
+{
+  try {
+    if (bind == nullptr) bind = PICHI_DEFAULT_BIND;
+    if (mmdb == nullptr) mmdb = PICHI_DEFAULT_MMDB;
+    auto server = api::Server{io, mmdb};
+    server.listen(bind, port);
+    io.run();
+    return 0;
+  }
+  catch (...) {
+    return -1;
+  }
+}
