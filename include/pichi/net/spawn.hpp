9d8475e384b206af6cec3add1f8299c6e680cbb9
Pichi <pichi@elude.in>
2019-02-15 22:27:25 +1100

    Replace asio::spawn in api::Server

--- include/pichi/net/spawn.hpp
+++ include/pichi/net/spawn.hpp
@@ -9,13 +9,14 @@
 namespace pichi::net {
 
 extern void logException(std::exception_ptr) noexcept;
-extern void stubHandler(std::exception_ptr) noexcept;
+extern void stubHandler(std::exception_ptr, boost::asio::yield_context) noexcept;
 
 template <typename T, typename Function, typename ExceptionHandler = decltype(stubHandler)>
 void spawn(T&& t, Function&& f, ExceptionHandler&& eh = stubHandler)
 {
   static_assert(std::is_invocable_v<std::decay_t<Function>, boost::asio::yield_context>);
-  static_assert(std::is_nothrow_invocable_v<std::decay_t<ExceptionHandler>, std::exception_ptr>);
+  static_assert(std::is_nothrow_invocable_v<std::decay_t<ExceptionHandler>, std::exception_ptr,
+                                            boost::asio::yield_context>);
   boost::asio::spawn(
       std::forward<T>(t),
       [f = std::forward<Function>(f), eh = std::forward<ExceptionHandler>(eh)](auto yield) mutable {
@@ -24,7 +25,7 @@ void spawn(T&& t, Function&& f, ExceptionHandler&& eh = stubHandler)
         }
         catch (...) {
           auto eptr = std::current_exception();
-          eh(eptr);
+          eh(eptr, yield);
           logException(eptr);
         }
       });
