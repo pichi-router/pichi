8da2e889602e280f5bcee469d006aef4287e7c7d
Pichi <pichi@elude.in>
2019-03-12 21:25:37 -0200

    Ignore logging HTTP EOF error

--- src/net/spawn.cpp
+++ src/net/spawn.cpp
@@ -1,9 +1,11 @@
+#include <boost/beast/http/error.hpp>
 #include <iostream>
 #include <pichi/exception.hpp>
 #include <pichi/net/spawn.hpp>
 
 using namespace std;
 namespace asio = boost::asio;
+namespace http = boost::beast::http;
 namespace sys = boost::system;
 
 namespace pichi::net {
@@ -17,7 +19,9 @@ void logException(std::exception_ptr eptr) noexcept
     cout << "Pichi Error: " << e.what() << endl;
   }
   catch (sys::system_error& e) {
-    if (e.code() == asio::error::eof || e.code() == asio::error::operation_aborted) return;
+    if (e.code() == asio::error::eof || e.code() == asio::error::operation_aborted ||
+        e.code() == http::error::end_of_stream)
+      return;
     cout << "Socket Error: " << e.what() << endl;
   }
 }
