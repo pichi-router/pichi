932f395ca0466799abb5cf4b7a8f4f7181d255e9
Pichi <pichi@elude.in>
2019-03-16 18:55:07 +0500

    Pull out APIs handling from net::Server

--- src/api/server.cpp
+++ src/api/server.cpp
@@ -1,8 +1,6 @@
-#include <algorithm>
 #include <boost/asio/ip/tcp.hpp>
-#include <boost/beast/core/flat_static_buffer.hpp>
+#include <boost/beast/core/flat_buffer.hpp>
 #include <boost/beast/http/read.hpp>
-#include <boost/beast/http/status.hpp>
 #include <boost/beast/http/write.hpp>
 #include <iostream>
 #include <memory>
@@ -10,220 +8,20 @@
 #include <pichi/api/vos.hpp>
 #include <pichi/asserts.hpp>
 #include <pichi/net/spawn.hpp>
-#include <rapidjson/stringbuffer.h>
-#include <rapidjson/writer.h>
-#include <sstream>
 
 using namespace std;
 namespace asio = boost::asio;
 namespace beast = boost::beast;
 namespace http = beast::http;
 namespace ip = asio::ip;
-namespace json = rapidjson;
 namespace sys = boost::system;
 using tcp = ip::tcp;
 
 namespace pichi::api {
 
-static auto const INGRESS_REGEX = regex{"^/ingresses/?([?#].*)?$"};
-static auto const INGRESS_NAME_REGEX = regex{"^/ingresses/([^?#/]+)/?([?#].*)?$"};
-static auto const EGRESS_REGEX = regex{"^/egresses/?([?#].*)?$"};
-static auto const EGRESS_NAME_REGEX = regex{"^/egresses/([^?#]+)/?([?#].*)?$"};
-static auto const RULE_REGEX = regex{"^/rules/?([?#].*)?$"};
-static auto const RULE_NAME_REGEX = regex{"^/rules/([^?#]+)/?([?#].*)?$"};
-static auto const ROUTE_REGEX = regex{"^/route/?([?#].*)?$"};
-
-static auto doc = json::Document{};
-static auto& alloc = doc.GetAllocator();
-
-static string serialize(json::Value const& v)
-{
-  auto buf = json::StringBuffer{};
-  auto writer = json::Writer<json::StringBuffer>{buf};
-  v.Accept(writer);
-  return buf.GetString();
-}
-
-static auto defaultResponse(http::status status)
-{
-  auto ret = Server::Response{};
-  ret.result(status);
-  ret.version(11);
-  if (status != http::status::no_content && status != http::status::not_found)
-    ret.set(http::field::content_type, "application/json");
-  return ret;
-}
-
-static bool matching(boost::string_view s, regex const& re, cmatch& r)
-{
-  return regex_match(s.data(), s.data() + s.size(), r, re);
-}
-
-static void writeWithoutException(tcp::socket& s, Server::Response& resp, asio::yield_context yield)
-{
-  auto ec = sys::error_code{};
-  http::async_write(s, resp, yield[ec]);
-  if (ec) cout << "Ignoring HTTP error: " << ec.message() << endl;
-}
-
-static void replyError(exception_ptr eptr, tcp::socket& s, asio::yield_context yield)
-{
-  auto reply = [&s, yield](auto&& vo, auto code) {
-    auto resp = defaultResponse(code);
-    resp.body() = serialize(toJson(vo, alloc));
-    writeWithoutException(s, resp, yield);
-  };
-  try {
-    if (eptr) rethrow_exception(eptr);
-  }
-  catch (Exception const& e) {
-    auto vo = ErrorVO{e.what()};
-    switch (e.error()) {
-    case PichiError::RES_IN_USE:
-      reply(vo, http::status::forbidden);
-      break;
-    case PichiError::BAD_JSON:
-      reply(vo, http::status::bad_request);
-      break;
-    case PichiError::SEMANTIC_ERROR:
-      reply(vo, http::status::unprocessable_entity);
-      break;
-    default:
-      reply(vo, http::status::internal_server_error);
-      break;
-    }
-  }
-  catch (sys::system_error const& e) {
-    reply(ErrorVO{e.what()}, e.code() == asio::error::address_in_use ?
-                                 http::status::locked :
-                                 http::status::internal_server_error);
-  }
-}
-
-template <typename InputIt>
-void dispatch(InputIt first, InputIt last, tcp::socket& s, asio::yield_context yield)
-{
-  auto buf = beast::flat_static_buffer<net::MAX_FRAME_SIZE>{};
-  auto req = Server::Request{};
-
-  http::async_read(s, buf, req, yield);
-  auto mr = cmatch{};
-  auto it = find_if(first, last, [v = req.method(), t = req.target(), &mr](auto&& item) {
-    return get<0>(item) == v && matching(t, get<1>(item), mr);
-  });
-  auto resp = it != last ? invoke(get<2>(*it), req, mr) : defaultResponse(http::status::not_found);
-  writeWithoutException(s, resp, yield);
-}
-
-template <typename Manager> auto getVO(Manager const& manager)
-{
-  auto resp = defaultResponse(http::status::ok);
-  resp.body() = serialize(toJson(begin(manager), end(manager), alloc));
-  return resp;
-}
-
-template <typename Manager>
-auto putVO(typename Manager::VO&& vo, cmatch const& mr, Manager& manager)
-{
-  manager.update(mr[1].str(), move(vo));
-  return defaultResponse(http::status::no_content);
-}
-
-template <typename Manager>
-auto putVO(Server::Request const& req, cmatch const& mr, Manager& manager)
-{
-  return putVO(parse<typename Manager::VO>(req.body()), mr, manager);
-}
-
-template <typename Manager> auto delVO(cmatch const& mr, Manager& manager)
-{
-  manager.erase(mr[1].str());
-  return defaultResponse(http::status::no_content);
-}
-
-static auto options(initializer_list<http::verb>&& verbs)
-{
-  auto oss = ostringstream{};
-  auto first = cbegin(verbs);
-  if (first != cend(verbs)) oss << *first++;
-  for_each(first, cend(verbs), [&oss](auto verb) { oss << "," << verb; });
-
-  auto resp = defaultResponse(http::status::no_content);
-  resp.set("Access-Control-Allow-Methods", oss.str());
-  return resp;
-}
-
 Server::Server(asio::io_context& io, char const* fn)
-  : strand_{io}, router_{fn}, eManager_{}, iManager_{strand_, router_, eManager_},
-    apis_{
-        make_tuple(http::verb::get, INGRESS_REGEX,
-                   [this](auto&&, auto&&) { return getVO(iManager_); }),
-        make_tuple(http::verb::options, INGRESS_REGEX,
-                   [](auto&&, auto&&) {
-                     return options({http::verb::get, http::verb::options});
-                   }),
-        make_tuple(http::verb::put, INGRESS_NAME_REGEX,
-                   [this](auto&& r, auto&& mr) { return putVO(r, mr, iManager_); }),
-        make_tuple(http::verb::delete_, INGRESS_NAME_REGEX,
-                   [this](auto&&, auto&& mr) { return delVO(mr, iManager_); }),
-        make_tuple(http::verb::options, INGRESS_NAME_REGEX,
-                   [](auto&&, auto&&) {
-                     return options({http::verb::put, http::verb::delete_, http::verb::options});
-                   }),
-        make_tuple(http::verb::get, EGRESS_REGEX,
-                   [this](auto&&, auto&&) { return getVO(eManager_); }),
-        make_tuple(http::verb::options, EGRESS_REGEX,
-                   [](auto&&, auto&&) {
-                     return options({http::verb::get, http::verb::options});
-                   }),
-        make_tuple(http::verb::put, EGRESS_NAME_REGEX,
-                   [this](auto&& r, auto&& mr) { return putVO(r, mr, eManager_); }),
-        make_tuple(http::verb::delete_, EGRESS_NAME_REGEX,
-                   [this](auto&&, auto&& mr) {
-                     assertFalse(router_.isUsed(mr[1].str()), PichiError::RES_IN_USE);
-                     return delVO(mr, eManager_);
-                   }),
-        make_tuple(http::verb::options, EGRESS_NAME_REGEX,
-                   [](auto&&, auto&&) {
-                     return options({http::verb::put, http::verb::delete_, http::verb::options});
-                   }),
-        make_tuple(http::verb::get, RULE_REGEX, [this](auto&&, auto&&) { return getVO(router_); }),
-        make_tuple(http::verb::options, RULE_REGEX,
-                   [](auto&&, auto&&) {
-                     return options({http::verb::get, http::verb::options});
-                   }),
-        make_tuple(
-            http::verb::put, RULE_NAME_REGEX,
-            [this](auto&& r, auto&& mr) { return putVO(parse<RuleVO>(r.body()), mr, router_); }),
-        make_tuple(http::verb::delete_, RULE_NAME_REGEX,
-                   [this](auto&&, auto&& mr) { return delVO(mr, router_); }),
-        make_tuple(http::verb::options, RULE_NAME_REGEX,
-                   [](auto&&, auto&&) {
-                     return options({http::verb::put, http::verb::delete_, http::verb::options});
-                   }),
-        make_tuple(http::verb::get, ROUTE_REGEX,
-                   [this](auto&&, auto&&) {
-                     auto resp = defaultResponse(http::status::ok);
-                     resp.body() = serialize(toJson(router_.getRoute(), alloc));
-                     return resp;
-                   }),
-        make_tuple(http::verb::put, ROUTE_REGEX,
-                   [this](auto&& r, auto&& mr) {
-                     auto vo = parse<RouteVO>(r.body());
-                     assertFalse(vo.default_.has_value() &&
-                                     eManager_.find(*vo.default_) == end(eManager_),
-                                 PichiError::SEMANTIC_ERROR, "Unknown egress"sv);
-                     assertTrue(all_of(cbegin(vo.rules_), cend(vo.rules_),
-                                       [this](auto&& pair) {
-                                         return eManager_.find(pair.second) != end(eManager_);
-                                       }),
-                                PichiError::SEMANTIC_ERROR, "Unknown egress"sv);
-                     router_.setRoute(move(vo));
-                     return defaultResponse(http::status::no_content);
-                   }),
-        make_tuple(http::verb::options, ROUTE_REGEX, [](auto&&, auto&&) {
-          return options({http::verb::get, http::verb::put, http::verb::options});
-        })}
+  : strand_{io}, router_{fn}, egresses_{},
+    ingresses_{strand_, router_, egresses_}, rest_{ingresses_, egresses_, router_}
 {
 }
 
@@ -235,10 +33,20 @@ void Server::listen(string_view address, uint16_t port)
       auto s = make_shared<tcp::socket>(a.async_accept(yield));
       net::spawn(
           strand_,
-          [first = cbegin(apis_), last = cend(apis_), s](auto yield) mutable {
-            dispatch(first, last, *s, yield);
+          [this, s](auto yield) mutable {
+            auto buf = beast::flat_buffer{};
+            auto req = Rest::Request{};
+            http::async_read(*s, buf, req, yield);
+
+            auto resp = rest_.handle(req);
+            http::async_write(*s, resp, yield);
           },
-          [s](auto eptr, auto yield) noexcept { replyError(eptr, *s, yield); });
+          [s](auto eptr, auto yield) noexcept {
+            auto ec = sys::error_code{};
+            auto resp = Rest::errorResponse(eptr);
+            http::async_write(*s, resp, yield[ec]);
+            if (ec) cout << "Ignoring HTTP error: " << ec.message() << endl;
+          });
     }
   });
 }
