84602d519027e050dfde2d3301e0ef220c52ffaa
Pichi <pichi@elude.in>
2019-03-20 14:25:32 -0500

    Change pichi::test::Socket from AsyncStream to SyncStream

--- include/pichi/test/socket.hpp
+++ include/pichi/test/socket.hpp
@@ -5,32 +5,29 @@
 
 #ifdef BUILD_TEST
 
-#include <boost/asio/async_result.hpp>
-#include <boost/asio/io_context.hpp>
 #include <boost/beast/core/flat_buffer.hpp>
+#include <boost/system/error_code.hpp>
 #include <functional>
 #include <pichi/asserts.hpp>
 
 namespace pichi::test {
 
-extern boost::asio::io_context io;
-
 /*
  * Faked socket class used for unit test. It's supposed to satisfy the following concepts at least:
- *   - AsyncReadStream: \
- *       https://www.boost.org/doc/libs/release/doc/html/boost_asio/reference/AsyncReadStream.html
- *   - AsyncWriteStream: \
- *       https://www.boost.org/doc/libs/release/doc/html/boost_asio/reference/AsyncWriteStream.html
+ *   - SyncReadStream: \
+ *       https://www.boost.org/doc/libs/release/doc/html/boost_asio/reference/SyncReadStream.html
+ *   - SyncWriteStream: \
+ *       https://www.boost.org/doc/libs/release/doc/html/boost_asio/reference/SyncWriteStream.html
  * w
  */
 class Socket {
 private:
   using ErrorCode = boost::system::error_code;
-  template <typename Handler>
-  using AsyncCompletion = boost::asio::async_completion<Handler, void(ErrorCode, size_t)>;
 
 public:
-  template <typename... Args> Socket(Args&&...) {}
+  template <typename... Args> explicit Socket(Args&&...) {}
+
+  explicit Socket(bool open) : open_{open} {}
 
   bool is_open() const { return open_; }
 
@@ -42,29 +39,37 @@ public:
 
   void connect() { open_ = true; }
 
-  template <typename MutableBufferSequence, typename ReadHandler>
-  auto async_read_some(MutableBufferSequence const& buf, ReadHandler&& h)
+  template <typename MutableBufferSequence> auto read_some(MutableBufferSequence const& buf)
   {
-    using boost::asio::buffer_copy;
     assertTrue(is_open());
-    auto init = AsyncCompletion<ReadHandler>{h};
-    std::invoke(init.completion_handler, ErrorCode{}, buffer_copy(buf, buffer_.data()));
-    return init.result.get();
+    assertTrue(boost::asio::buffer_size(buf) <= buffer_.size());
+    auto copied = boost::asio::buffer_copy(buf, buffer_.data());
+    buffer_.consume(copied);
+    return copied;
   }
 
-  template <typename ConstBufferSequence, typename WriteHandler>
-  auto async_write_some(ConstBufferSequence const& buf, WriteHandler&& h)
+  template <typename MutableBufferSequence>
+  auto read_some(MutableBufferSequence const& buf, ErrorCode& ec)
+  {
+    ec = {};
+    return read_some(buf);
+  }
+
+  template <typename ConstBufferSequence> auto write_some(ConstBufferSequence const& buf)
   {
     assertTrue(is_open());
     auto copied = boost::asio::buffer_size(buf);
     boost::asio::buffer_copy(buffer_.prepare(copied), buf);
     buffer_.commit(copied);
-    auto init = AsyncCompletion<WriteHandler>{h};
-    std::invoke(init.completion_handler, ErrorCode{}, copied);
-    return init.result.get();
+    return copied;
   }
 
-  auto get_executor() { return io.get_executor(); }
+  template <typename ConstBufferSequence>
+  auto write_some(ConstBufferSequence const& buf, ErrorCode& ec)
+  {
+    ec = {};
+    return write_some(buf);
+  }
 
 private:
   boost::beast::flat_buffer buffer_;
