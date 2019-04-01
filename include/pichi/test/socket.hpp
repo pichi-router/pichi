a39081eb31073d05b4fc23f1a75142c76cd4de45
Pichi <pichi@elude.in>
2019-04-01 05:43:56 -1100

    Add test::Socket::fill/flush for testing

--- include/pichi/test/socket.hpp
+++ include/pichi/test/socket.hpp
@@ -5,29 +5,72 @@
 
 #ifdef BUILD_TEST
 
+#include <algorithm>
+#include <boost/asio/buffers_iterator.hpp>
 #include <boost/beast/core/flat_buffer.hpp>
 #include <boost/system/error_code.hpp>
-#include <functional>
+#include <initializer_list>
 #include <pichi/asserts.hpp>
 
 namespace pichi::test {
 
+class Socket {
+private:
+  using Buffer = boost::beast::flat_buffer;
+
+  template <typename OutputIt> static auto read(Buffer& buf, OutputIt it, size_t n)
+  {
+    assertTrue(n <= buf.size());
+    std::copy_n(boost::asio::buffers_begin(buf.data()), n, it);
+    buf.consume(n);
+    return n;
+  }
+
+  template <typename InputIt> static auto write(Buffer& buf, InputIt it, size_t n)
+  {
+    std::copy_n(it, n, boost::asio::buffers_begin(buf.prepare(n)));
+    buf.commit(n);
+    return n;
+  }
+
+public:
+  template <typename MutableBufferSequence> auto read(MutableBufferSequence const& buf)
+  {
+    return read(rBuf_, boost::asio::buffers_begin(buf), boost::asio::buffer_size(buf));
+  }
+
+  template <typename ConstBufferSequence> auto write(ConstBufferSequence const& buf)
+  {
+    return write(wBuf_, boost::asio::buffers_begin(buf), boost::asio::buffer_size(buf));
+  }
+
+  auto fill(std::initializer_list<uint8_t> l) { return write(rBuf_, std::cbegin(l), l.size()); }
+
+  auto fill(ConstBuffer<uint8_t> buf) { return write(rBuf_, std::cbegin(buf), buf.size()); }
+
+  auto flush(MutableBuffer<uint8_t> buf) { return read(wBuf_, std::cbegin(buf), buf.size()); }
+
+  auto available() const { return wBuf_.size(); }
+
+private:
+  Buffer rBuf_;
+  Buffer wBuf_;
+};
+
 /*
- * Faked socket class used for unit test. It's supposed to satisfy the following concepts at least:
+ * Faked stream class used for unit test. It's supposed to satisfy the following concepts at least:
  *   - SyncReadStream: \
  *       https://www.boost.org/doc/libs/release/doc/html/boost_asio/reference/SyncReadStream.html
  *   - SyncWriteStream: \
  *       https://www.boost.org/doc/libs/release/doc/html/boost_asio/reference/SyncWriteStream.html
  * w
  */
-class Socket {
+class Stream {
 private:
   using ErrorCode = boost::system::error_code;
 
 public:
-  template <typename... Args> explicit Socket(Args&&...) {}
-
-  explicit Socket(bool open) : open_{open} {}
+  explicit Stream(Socket& socket, bool open = false) : socket_{socket}, open_{open} {}
 
   bool is_open() const { return open_; }
 
@@ -42,10 +85,7 @@ public:
   template <typename MutableBufferSequence> auto read_some(MutableBufferSequence const& buf)
   {
     assertTrue(is_open());
-    assertTrue(boost::asio::buffer_size(buf) <= buffer_.size());
-    auto copied = boost::asio::buffer_copy(buf, buffer_.data());
-    buffer_.consume(copied);
-    return copied;
+    return socket_.read(buf);
   }
 
   template <typename MutableBufferSequence>
@@ -58,10 +98,7 @@ public:
   template <typename ConstBufferSequence> auto write_some(ConstBufferSequence const& buf)
   {
     assertTrue(is_open());
-    auto copied = boost::asio::buffer_size(buf);
-    boost::asio::buffer_copy(buffer_.prepare(copied), buf);
-    buffer_.commit(copied);
-    return copied;
+    return socket_.write(buf);
   }
 
   template <typename ConstBufferSequence>
@@ -72,8 +109,8 @@ public:
   }
 
 private:
-  boost::beast::flat_buffer buffer_;
-  bool open_ = false;
+  Socket& socket_;
+  bool open_;
 };
 
 } // namespace pichi::test
