7b5d4dd2c80368ac12ba6c2f87505048b2d03fc6
Pichi <pichi@elude.in>
2019-05-05 04:48:04 +0500

    Fix build failure with libsodium 1.0.17

--- cmake/GenerateConfig.cmake
+++ cmake/GenerateConfig.cmake
@@ -1,5 +1,12 @@
 message(STATUS "Generating config.h")
 
+if (Sodium_VERSION_STRING VERSION_GREATER_EQUAL "1.0.17")
+  # From libsodium 1.0.17, the declarations of crypto_stream_xxx functions contain
+  #   '__attribute__ ((nonnull))', which might let GCC cause '-Wignored-attributes' warning
+  #   if using std::is_same to detect function signature equation.
+  set(NO_IGNORED_ATTRIBUTES_FOR_SODIUM ON)
+endif (Sodium_VERSION_STRING VERSION_GREATER_EQUAL "1.0.17")
+
 configure_file(${CMAKE_SOURCE_DIR}/include/config.h.in ${CMAKE_BINARY_DIR}/include/config.h)
 
 message(STATUS "Generating config.h - done")
--- src/crypto/stream.cpp
+++ src/crypto/stream.cpp
@@ -1,3 +1,4 @@
+#include "config.h"
 #include <iostream>
 #include <pichi/asserts.hpp>
 #include <pichi/common.hpp>
@@ -16,9 +17,21 @@ static size_t sodiumHelper(SodiumFunc&& func, ConstBuffer<uint8_t> key, size_t o
 {
   assertTrue(output.size() >= input.size(), PichiError::MISC);
 
+#ifdef NO_IGNORED_ATTRIBUTES_FOR_SODIUM
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wignored-attributes"
+#endif // __GNUC__
+#endif // NO_IGNORED_ATTRIBUTES_FOR_SODIUM
   using IC =
       conditional_t<is_same_v<decay_t<SodiumFunc>, decltype(&crypto_stream_chacha20_ietf_xor_ic)>,
                     uint32_t, uint64_t>;
+#ifdef NO_IGNORED_ATTRIBUTES_FOR_SODIUM
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif // __GNUC__
+#endif // NO_IGNORED_ATTRIBUTES_FOR_SODIUM
+
   auto ic = static_cast<IC>(offset / 64);
   auto padding = offset % 64;
   auto converted = min((64 - padding) % 64, input.size());
