f0c7cb48a1c4c3c11793a365073dbf835b5d1ed8
Pichi <pichi@elude.in>
2019-05-24 05:59:22 -1000

    Fix MSVC failure caused by no copy elision support in member initializer list

--- include/pichi/net/socks5.hpp
+++ include/pichi/net/socks5.hpp
@@ -5,56 +5,20 @@
 #include <pichi/common.hpp>
 #include <pichi/net/adapter.hpp>
 #include <string>
-#include <type_traits>
 #include <unordered_map>
 
 namespace pichi::net {
 
-namespace detail {
-
-template <typename First, typename Second> struct Helper {
-  template <typename T>
-  inline constexpr static bool IsFirst = std::is_same_v<First, std::decay_t<T>> ||
-                                         std::is_same_v<std::optional<First>, std::decay_t<T>>;
-
-  template <typename Arg0, typename... Args> static auto constructFirst(Arg0&& arg0, Args&&...)
-  {
-    if constexpr (IsFirst<Arg0>) {
-      return std::optional<First>{std::forward<Arg0>(arg0)};
-    }
-    else {
-      suppressC4100(std::forward<Arg0>(arg0));
-      return std::optional<First>{};
-    }
-  }
-
-  template <typename Arg0, typename... Args>
-  static auto constructSecond(Arg0&& arg0, Args&&... args)
-  {
-    if constexpr (IsFirst<Arg0>) {
-      suppressC4100(std::forward<Arg0>(arg0));
-      return Second{std::forward<Args>(args)...};
-    }
-    else {
-      return Second{std::forward<Arg0>(arg0), std::forward<Args>(args)...};
-    }
-  }
-};
-
-} // namespace detail
-
 template <typename Stream> class Socks5Ingress : public Ingress {
 private:
   using Credentials = std::unordered_map<std::string, std::string>;
-  using Constructor = detail::Helper<Credentials, Stream>;
 
   void authenticate(Yield);
 
 public:
   template <typename... Args>
-  Socks5Ingress(Args&&... args)
-    : stream_{Constructor::constructSecond(std::forward<Args>(args)...)},
-      credentials_{Constructor::constructFirst(std::forward<Args>(args)...)}
+  Socks5Ingress(std::optional<Credentials> credentials, Args&&... args)
+    : stream_{std::forward<Args>(args)...}, credentials_{std::move(credentials)}
   {
   }
 
@@ -75,15 +39,13 @@ private:
 template <typename Stream> class Socks5Egress : public Egress {
 private:
   using Credential = std::pair<std::string, std::string>;
-  using Constructor = detail::Helper<Credential, Stream>;
 
   void authenticate(Yield);
 
 public:
   template <typename... Args>
-  Socks5Egress(Args&&... args)
-    : stream_{Constructor::constructSecond(std::forward<Args>(args)...)},
-      credential_{Constructor::constructFirst(std::forward<Args>(args)...)}
+  Socks5Egress(std::optional<Credential> credential, Args&&... args)
+    : stream_{std::forward<Args>(args)...}, credential_{std::move(credential)}
   {
   }
 
