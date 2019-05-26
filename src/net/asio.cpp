4cebe614e90c637d22d1c637958eb463f7c81816
Pichi <pichi@elude.in>
2019-05-26 03:12:39 -0100

    Fix build failure caused by Boost.Asio 1.70.0 having changed its API

--- src/net/asio.cpp
+++ src/net/asio.cpp
@@ -93,8 +93,12 @@ void connect(Endpoint const& endpoint, Socket& s, Yield yield)
   else
 #endif // BUILD_TEST
     asio::async_connect(s,
-                        tcp::resolver{s.get_executor().context()}.async_resolve(
-                            endpoint.host_, endpoint.port_, yield),
+                        tcp::resolver{s.get_executor()
+#ifndef RESOLVER_CONSTRUCTED_FROM_EXECUTOR
+                                          .context()
+#endif // RESOLVER_CONSTRUCTED_FROM_EXECUTOR
+                        }
+                            .async_resolve(endpoint.host_, endpoint.port_, yield),
                         yield);
 }
 
