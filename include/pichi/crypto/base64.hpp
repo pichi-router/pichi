3e455faf7f6262ba2bea5bc4a04e72fbac797ae9
Pichi <pichi@elude.in>
2019-07-28 08:44:41 -1100

    Implement specific disconnect for HttpIngress

--- include/pichi/crypto/base64.hpp
+++ include/pichi/crypto/base64.hpp
@@ -1,6 +1,7 @@
 #ifndef PICHI_CRYPTO_BASE64_HPP
 #define PICHI_CRYPTO_BASE64_HPP
 
+#include <pichi/exception.hpp>
 #include <stdint.h>
 #include <string>
 #include <string_view>
@@ -8,7 +9,7 @@
 namespace pichi::crypto {
 
 extern std::string base64Encode(std::string_view);
-extern std::string base64Decode(std::string_view);
+extern std::string base64Decode(std::string_view, PichiError = PichiError::MISC);
 
 } // namespace pichi::crypto
 
