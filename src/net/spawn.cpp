4adb6088109c42f4e0f9184eb53dba4d9104ccf1
Pichi <pichi@elude.in>
2020-08-26 05:14:48 +1000

    Fix building failure caused by -Wdeprecated-declarations warning complained by Clang11

--- src/net/spawn.cpp
+++ src/net/spawn.cpp
@@ -1,3 +1,5 @@
+#include <pichi/config.hpp>
+// Include config.hpp first
 #include <boost/beast/http/error.hpp>
 #include <iostream>
 #include <pichi/exception.hpp>
@@ -28,4 +30,4 @@ void logException(std::exception_ptr eptr) noexcept
 
 void stubHandler(std::exception_ptr, asio::yield_context) noexcept {}
 
-} // namespace pichi::net
+}  // namespace pichi::net
