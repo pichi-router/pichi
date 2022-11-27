1e9d4c8a6c71787ad073a1fb046e5c377656095b
Pichi <pichi@elude.in>
2022-11-27 00:58:09 -0800

    Remove all global setting macros in cmake

--- cmake/Configure.cmake
+++ cmake/Configure.cmake
@@ -1,6 +1,3 @@
-# C/C++ Macros
-add_compile_definitions(BOOST_ASIO_NO_DEPRECATED)
-
 if(TRANSPARENT_PF AND APPLE)
   message(STATUS "Downloading the essential Darwin-XNU header files")
   file(DOWNLOAD
@@ -12,56 +9,7 @@ if(TRANSPARENT_PF AND APPLE)
   file(DOWNLOAD
     "https://opensource.apple.com/source/xnu/xnu-7195.81.3/bsd/net/radix.h"
     "${CMAKE_BINARY_DIR}/include/net/radix.h")
-  add_compile_definitions(PRIVATE)
   message(STATUS "Downloading the essential Darwin-XNU header files -- done")
-endif()
-
-# Find out whether class template argument deduction is supported for std::array
-message(STATUS "Checking P0702R1 feature")
-try_compile(HAS_CLASS_TEMPLATE_ARGUMENT_DEDUCTION
-  ${CMAKE_BINARY_DIR}/cmake
-  ${CMAKE_SOURCE_DIR}/cmake/test/P0702R1.cpp)
-
-if(HAS_CLASS_TEMPLATE_ARGUMENT_DEDUCTION)
-  message(STATUS "Checking P0702R1 feature - yes")
-else()
-  message(STATUS "Checking P0702R1 feature - no")
-endif()
-
-# Enable complaining all warnings as errors
-if(MSVC)
-  add_compile_options(/W4 /WX)
-else()
-  add_compile_options(-Wall -Wextra -Werror)
-endif()
-
-# Options for Microsoft C++
-if(MSVC)
-  # In MSVC, the default Exception Handling option is /EHsc, Which wouldn't catch exceptions
-  # thrown by boost::context. '/EHs' would make it correctly at least.
-  # Further information:
-  # - https://docs.microsoft.com/en-us/cpp/build/reference/eh-exception-handling-model?view=vs-2017
-  # - https://www.boost.org/doc/libs/release/libs/context/doc/html/context/requirements.html
-  add_compile_options(/EHs)
-
-  # Avoid C1128 error
-  add_compile_options(/bigobj)
-
-  # Avoid warning STL4015, caused by rapidjson
-  add_compile_definitions(_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING)
-
-  # Avoid warning STL4009, which should be incorrectly complained.
-  # https://github.com/chriskohlhoff/asio/issues/290
-  if(MSVC_VERSION VERSION_LESS "1920")
-    add_compile_definitions(_SILENCE_CXX17_ALLOCATOR_VOID_DEPRECATION_WARNING)
-  endif()
-endif()
-
-if(WIN32 AND CMAKE_SYSTEM_VERSION)
-  string(REGEX REPLACE "^([0-9]+)\.[0-9]+\.[0-9]+$" "\\1" major ${CMAKE_SYSTEM_VERSION})
-  string(REGEX REPLACE "^[0-9]+\.([0-9]+)\.[0-9]+$" "\\1" minor ${CMAKE_SYSTEM_VERSION})
-  math(EXPR win32_version "(${major} << 8) + ${minor}" OUTPUT_FORMAT HEXADECIMAL)
-  add_compile_definitions(_WIN32_WINNT=${win32_version})
 endif()
 
 # Generating config.hpp
