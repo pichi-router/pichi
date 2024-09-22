b762ca6f761f0f4f0fa1b6f564a1ebc667165462
Pichi <pichi-router@proton.me>
2024-09-22 08:42:22 +0100

    Implement a rudimentary prototype of actor::Router

---
+++ include/pichi/actor/router.hpp
@@ -0,0 +1,47 @@
+#ifndef PICHI_ACTOR_ROUTER_HPP
+#define PICHI_ACTOR_ROUTER_HPP
+
+#include <pichi/actor/common.hpp>
+#include <pichi/vo/egress.hpp>
+#include <pichi/vo/ingress.hpp>
+#include <pichi/vo/route.hpp>
+#include <pichi/vo/rule.hpp>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+namespace pichi::actor {
+
+class Router {
+private:
+  using Pointer = std::shared_ptr<Router>;
+
+  using Egresses = std::unordered_map<std::string, vo::Egress>;
+  using Rules    = std::unordered_map<std::string, vo::Rule>;
+
+public:
+  Router(IOExecutor);
+
+  std::string get_egresses() const;
+  Pointer     del_egress(std::string const&);
+  Pointer     put_egress(std::string const&, std::string_view);
+
+  std::string get_rules() const;
+  Pointer     del_rule(std::string const&);
+  Pointer     put_rule(std::string const&, std::string_view);
+
+  std::string get_route() const;
+  Pointer     put_route(std::string_view);
+
+  Awaitable<vo::Egress> route(vo::Ingress const&) const;
+
+private:
+  IOExecutor ex_;
+  Egresses   egresses_;
+  Rules      rules_ = {};
+  vo::Route  route_;
+};
+
+}  // namespace pichi::actor
+
+#endif  // PICHI_ACTOR_ROUTER_HPP
---
+++ src/actor/router.cpp
@@ -0,0 +1,85 @@
+#include <pichi/common/config.hpp>
+// Include config.hpp first
+#include <algorithm>
+#include <pichi/actor/router.hpp>
+#include <pichi/vo/parse.hpp>
+#include <pichi/vo/to_json.hpp>
+#include <ranges>
+
+using namespace std::literals;
+namespace asio = boost::asio;
+namespace json = rapidjson;
+namespace rngs = std::ranges;
+namespace sys  = boost::system;
+
+namespace pichi::actor {
+
+static auto const DEFAULT_EGRESS_NAME = "direct"s;
+
+template <rngs::range Range> std::string to_string(Range const& range)
+{
+  auto alloc = json::Document::AllocatorType{};
+  return vo::toString(vo::toJson(rngs::cbegin(range), rngs::cend(range), alloc));
+}
+
+Router::Router(IOExecutor ex) : 
+  ex_{std::move(ex)},
+  egresses_{{DEFAULT_EGRESS_NAME, vo::Egress{.type_ = AdapterType::DIRECT}}},
+  route_{.default_ = DEFAULT_EGRESS_NAME}
+{
+}
+
+std::string Router::get_egresses() const { return to_string(egresses_); }
+
+Router::Pointer Router::del_egress(std::string const& name)
+{
+  // TODO check if locked
+  auto rt = std::make_shared<Router>(*this);
+  rt->egresses_.erase(name);
+  return rt;
+}
+
+Router::Pointer Router::put_egress(std::string const& name, std::string_view value)
+{
+  auto vo = vo::parse<vo::Egress>(value);
+  auto rt = std::make_shared<Router>(*this);
+
+  rt->egresses_[name] = std::move(vo);
+  return rt;
+}
+
+std::string Router::get_rules() const { return to_string(rules_); }
+
+Router::Pointer Router::del_rule(std::string const& name)
+{
+  // TODO check if locked
+  auto rt = std::make_shared<Router>(*this);
+  rt->rules_.erase(name);
+  return rt;
+}
+
+Router::Pointer Router::put_rule(std::string const& name, std::string_view value)
+{
+  auto vo = vo::parse<vo::Rule>(value);
+  auto rt = std::make_shared<Router>(*this);
+
+  rt->rules_[name] = std::move(vo);
+  return rt;
+}
+
+std::string Router::get_route() const
+{
+  auto alloc = json::Document::AllocatorType{};
+  return vo::toString(vo::toJson(route_, alloc));
+}
+
+Router::Pointer Router::put_route(std::string_view value)
+{
+  auto vo = vo::parse<vo::Route>(value);
+  auto rt = std::make_shared<Router>(*this);
+
+  rt->route_ = std::move(vo);
+  return rt;
+}
+
+}  // namespace pichi::actor
